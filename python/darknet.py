720c39bb1b4f9e9e1c51d07793009d4dc8967675
Shun Git <kwokshungit@gmail.com>
2018-10-22 16:38:44 +0800

    add run_openimages.py

--- python/darknet.py
+++ python/darknet.py
@@ -1,156 +1,112 @@
-from ctypes import *
-import math
-import random
+from os.path import abspath
+from ctypes import CDLL, RTLD_GLOBAL, POINTER, Structure
+from ctypes import c_float, c_int, c_void_p, c_char_p, pointer
 
-def sample(probs):
-    s = sum(probs)
-    probs = [a/s for a in probs]
-    r = random.uniform(0, 1)
-    for i in range(len(probs)):
-        r = r - probs[i]
-        if r <= 0:
-            return i
-    return len(probs)-1
+lib = CDLL(abspath('libdarknet.so'), RTLD_GLOBAL)
 
-def c_array(ctype, values):
-    arr = (ctype*len(values))()
-    arr[:] = values
-    return arr
 
 class BOX(Structure):
-    _fields_ = [("x", c_float),
-                ("y", c_float),
-                ("w", c_float),
-                ("h", c_float)]
+    _fields_ = (
+        ("x", c_float), ("y", c_float), ("w", c_float), ("h", c_float)
+    )
+
 
 class DETECTION(Structure):
-    _fields_ = [("bbox", BOX),
-                ("classes", c_int),
-                ("prob", POINTER(c_float)),
-                ("mask", POINTER(c_float)),
-                ("objectness", c_float),
-                ("sort_class", c_int)]
+    _fields_ = (
+        ("bbox", BOX), ("classes", c_int), ("prob", POINTER(c_float)),
+        ("mask", POINTER(c_float)), ("objectness", c_float),
+        ("sort_class", c_int)
+    )
 
 
 class IMAGE(Structure):
-    _fields_ = [("w", c_int),
-                ("h", c_int),
-                ("c", c_int),
-                ("data", POINTER(c_float))]
+    _fields_ = (
+        ("w", c_int), ("h", c_int), ("c", c_int), ("data", POINTER(c_float))
+    )
+
 
 class METADATA(Structure):
-    _fields_ = [("classes", c_int),
-                ("names", POINTER(c_char_p))]
+    _fields_ = (("classes", c_int), ("names", POINTER(c_char_p)))
 
-    
 
-#lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
-lib.network_width.argtypes = [c_void_p]
-lib.network_width.restype = c_int
-lib.network_height.argtypes = [c_void_p]
-lib.network_height.restype = c_int
-
-predict = lib.network_predict
-predict.argtypes = [c_void_p, POINTER(c_float)]
-predict.restype = POINTER(c_float)
-
-set_gpu = lib.cuda_set_device
-set_gpu.argtypes = [c_int]
-
-make_image = lib.make_image
-make_image.argtypes = [c_int, c_int, c_int]
-make_image.restype = IMAGE
-
-get_network_boxes = lib.get_network_boxes
-get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float, POINTER(c_int), c_int, POINTER(c_int)]
-get_network_boxes.restype = POINTER(DETECTION)
-
-make_network_boxes = lib.make_network_boxes
-make_network_boxes.argtypes = [c_void_p]
-make_network_boxes.restype = POINTER(DETECTION)
-
-free_detections = lib.free_detections
-free_detections.argtypes = [POINTER(DETECTION), c_int]
-
-free_ptrs = lib.free_ptrs
-free_ptrs.argtypes = [POINTER(c_void_p), c_int]
-
-network_predict = lib.network_predict
-network_predict.argtypes = [c_void_p, POINTER(c_float)]
-
-reset_rnn = lib.reset_rnn
-reset_rnn.argtypes = [c_void_p]
+predict = lib.network_predict_image
+predict.argtypes = (c_void_p, IMAGE)
 
 load_net = lib.load_network
-load_net.argtypes = [c_char_p, c_char_p, c_int]
 load_net.restype = c_void_p
 
-do_nms_obj = lib.do_nms_obj
-do_nms_obj.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+load_meta = lib.get_metadata
+load_meta.restype = METADATA
 
-do_nms_sort = lib.do_nms_sort
-do_nms_sort.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+load_image = lib.load_image_color
+load_image.argtypes = (c_char_p, c_int, c_int)
+load_image.restype = IMAGE
 
 free_image = lib.free_image
 free_image.argtypes = [IMAGE]
 
-letterbox_image = lib.letterbox_image
-letterbox_image.argtypes = [IMAGE, c_int, c_int]
-letterbox_image.restype = IMAGE
+free_detections = lib.free_detections
+free_detections.argtypes = (POINTER(DETECTION), c_int)
 
-load_meta = lib.get_metadata
-lib.get_metadata.argtypes = [c_char_p]
-lib.get_metadata.restype = METADATA
+do_nms = lib.do_nms_obj
+do_nms.argtypes = (POINTER(DETECTION), c_int, c_int, c_float)
 
-load_image = lib.load_image_color
-load_image.argtypes = [c_char_p, c_int, c_int]
-load_image.restype = IMAGE
+get_boxes = lib.get_network_boxes
+get_boxes.argtypes = (
+    c_void_p, c_int, c_int, c_float, c_float,
+    POINTER(c_int), c_int, POINTER(c_int)
+)
+get_boxes.restype = POINTER(DETECTION)
 
-rgbgr_image = lib.rgbgr_image
-rgbgr_image.argtypes = [IMAGE]
 
-predict_image = lib.network_predict_image
-predict_image.argtypes = [c_void_p, IMAGE]
-predict_image.restype = POINTER(c_float)
+class Darknet:
 
-def classify(net, meta, im):
-    out = predict_image(net, im)
-    res = []
-    for i in range(meta.classes):
-        res.append((meta.names[i], out[i]))
-    res = sorted(res, key=lambda x: -x[1])
-    return res
+    def __init__(self, meta, cfg, weights):
+        self.net = load_net(cfg.encode(), weights.encode(), 0)
+        self.meta = load_meta(meta.encode())
 
-def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
-    im = load_image(image, 0, 0)
-    num = c_int(0)
-    pnum = pointer(num)
-    predict_image(net, im)
-    dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, None, 0, pnum)
-    num = pnum[0]
-    if (nms): do_nms_obj(dets, num, meta.classes, nms);
+    def name(self, i):
+        return self.meta.names[i].decode()
 
-    res = []
-    for j in range(num):
-        for i in range(meta.classes):
-            if dets[j].prob[i] > 0:
-                b = dets[j].bbox
-                res.append((meta.names[i], dets[j].prob[i], (b.x, b.y, b.w, b.h)))
-    res = sorted(res, key=lambda x: -x[1])
-    free_image(im)
-    free_detections(dets, num)
-    return res
-    
+    def __call__(self, fn, thresh=.1, hier=.5, nms=.45):
+        img = load_image(fn.encode(), 0, 0)
+        w = img.w
+        h = img.h
+        n = pointer(c_int(0))
+
+        predict(self.net, img)
+        free_image(img)
+
+        dets = get_boxes(self.net, w, h, thresh, hier, None, 0, n)
+        num_dets = n[0]
+
+        if nms:
+            do_nms(dets, num_dets, self.meta.classes, nms)
+
+        for j in range(num_dets):
+            d = dets[j]
+            for i in range(self.meta.classes):
+                conf = d.prob[i]
+                if not conf:
+                    continue
+                b = d.bbox
+                yield conf, i, (b.x / w, b.y / h, b.w / w, b.h / h)
+
+        free_detections(dets, num_dets)
+
+
 if __name__ == "__main__":
-    #net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
-    #im = load_image("data/wolf.jpg", 0, 0)
-    #meta = load_meta("cfg/imagenet1k.data")
-    #r = classify(net, meta, im)
-    #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
-    meta = load_meta("cfg/coco.data")
-    r = detect(net, meta, "data/dog.jpg")
-    print r
-    
+    import sys
+    if len(sys.argv) < 3:
+        print('Usage: %s netname file')
+        sys.exit(1)
 
+    net = sys.argv[1]
+    fn = sys.argv[2]
+
+    dn = Darknet(*(a % net for a in ('%s.data', '%s.cfg', '%s.weights')))
+    for c, i, b in sorted(dn(fn), reverse=True):
+        print('\t%s (%.3f)' % (dn.name(i), c))
+        print('%d %s' % (i, ' '.join('%.6f' % p for p in b)))
+
+    print('\nDone')
---
+++ python/run_openimages.py
@@ -0,0 +1,54 @@
+from darknet import Darknet
+
+net = (
+    'cfg/openimages.data',
+    'cfg/yolov3-openimages.cfg',
+    'openimages.weights'
+)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print('Usage: %s file' % sys.argv[0])
+        sys.exit(1)
+
+    fn = sys.argv[1]
+
+    from os import walk
+    from time import perf_counter
+    from os.path import isdir, exists, join
+
+    filenames = []
+
+    if isdir(fn):
+        dp, _, fn = next(walk(fn))
+        filenames.extend(join(dp, f) for f in fn)
+    elif exists(fn):
+        filenames.append(fn)
+
+    dn = Darknet(*net)
+    total = len(filenames)
+    mag = len(str(total))
+
+    sn = 1
+    start = perf_counter()
+
+    for fn in filenames:
+        t_fn = fn.split('.')[0]
+        l_fn = t_fn + '.txt'
+
+        if exists(l_fn):
+            continue
+
+        cinfo = '{:{pad}d}/{}'.format(sn, total, pad=mag)
+        t = perf_counter()
+        with open(l_fn, 'w') as lb:
+            for c, i, b in sorted(dn(fn), reverse=True):
+                # print('\t%s (%.3f)' % (dn.name(i), c))
+                print('%d %s' % (i, ' '.join('%.6f' % p for p in b)), file=lb)
+        now = perf_counter()
+        tinfo = '%2.3fs (%.1f/s)' % (now - t, sn / (now - start))
+        print('\r%-12s| %-60s| %-10s' % (cinfo, t_fn, tinfo), end='')
+        sn += 1
+
+    print('\nDone')
